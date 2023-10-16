b666518afb198f92b214d0cd0b257380838b30a4
Air-KS <kevinrogeret@gmail.com>
2023-10-16 12:03:40 +0200

    Amélioration du code pour une meilleure lisibilité et ajout de commentaires

--- Application/src/components/footer.js
+++ Application/src/components/footer.js
@@ -1,63 +1,69 @@
 // src/components/footer.js
 
+// Importation des dépendances nécessaires
 import React from "react";
 import { View, Text, Linking, TouchableOpacity } from "react-native";
 import stylesFooter from "../styles/footer";
 
+// Composant Footer qui affiche le pied de page de l'application
 const Footer = () => {
 
+  // Fonction pour ouvrir une URL dans le navigateur par défaut
   const openURL = (url) => {
     Linking.canOpenURL(url).then(supported => {
       if (supported) {
         Linking.openURL(url);
       } else {
-        console.log("Ne peu pas ouvrire l'URL" + url);
+        console.log("Ne peut pas ouvrir l'URL: " + url);
       }
     });
   };
 
+  // Fonction pour envoyer un e-mail en utilisant le client de messagerie par défaut
   const sendEmail = (email, subject = '', body = '') => {
     const url = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
     openURL(url);
   };
 
   return (
+    // Conteneur principal du pied de page
     <View style={stylesFooter.footerContainer}>
 
-
+      {/* Section d'information à gauche */}
       <View style={stylesFooter.leftContainer}>
-      <Text style={stylesFooter.title}>Pour en savoir plus :</Text>
-      <Text style={stylesFooter.leftText}>
-      Cette application est actuellement en développement. {'\n'}
-      Elle est réalisée dans le cadre de l'école Holberton School
-      et conclut la fin de la première année.
-      </Text>
-
+        <Text style={stylesFooter.title}>Pour en savoir plus :</Text>
+        <Text style={stylesFooter.leftText}>
+          Cette application est actuellement en développement. {'\n'}
+          Elle est réalisée dans le cadre de l'école Holberton School
+          et conclut la fin de la première année.
+        </Text>
       </View>
 
+      {/* Barre latérale de séparation */}
       <View style={stylesFooter.sidebar}></View>
 
+      {/* Section de contact à droite */}
       <View style={stylesFooter.rightContainer}>
+        <Text style={stylesFooter.title}>Nous contacter :</Text>
 
-      <Text style={stylesFooter.title}>Nous contacter :</Text>
-      <Text style={stylesFooter.rightText}>Mail : {'\n'}
-      <TouchableOpacity onPress={() => sendEmail('zanamelodie@gmail.com')}>
+        {/* Liens d'e-mails pour contacter les développeurs */}
+        <Text style={stylesFooter.rightText}>Mail : {'\n'}
+          <TouchableOpacity onPress={() => sendEmail('zanamelodie@gmail.com')}>
             <Text style={{color: 'blue'}}>zanamelodie@gmail.com</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => sendEmail('sebastienlenne@gmail.com')}>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => sendEmail('sebastienlenne@gmail.com')}>
             <Text style={{color: 'blue'}}>sebastienlenne@gmail.com{'\n'}</Text>
-      </TouchableOpacity>
-
-      <Text>Notre GitHub :{'\n'}</Text>
-
-      <TouchableOpacity onPress={() => openURL('https://github.com/Air-KS/HolyConnect')}>
+          </TouchableOpacity>
+          {/* Lien vers le dépôt GitHub du projet */}
+          <Text>Notre GitHub :{'\n'}</Text>
+          <TouchableOpacity onPress={() => openURL('https://github.com/Air-KS/HolyConnect')}>
             <Text style={{color: 'blue'}}>HolyConnect</Text>
-      </TouchableOpacity>
-
-      </Text>
+          </TouchableOpacity>
+        </Text>
       </View>
     </View>
   )
 }
 
+// Exportation du composant Footer pour une utilisation dans d'autres fichiers
 export default Footer;
--- Application/src/components/menuProfil.js
+++ Application/src/components/menuProfil.js
@@ -1,59 +1,102 @@
 // src/components/menuProfil.js
+
+// Importation des dépendances nécessaires
 import React, { useContext } from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { AuthContext } from '../contexts/AuthContext';
-import { useNavigation } from '@react-navigation/native';
-import stylesMenuProfil from '../styles/stylesMenuProfil';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import menuProfil from '../styles/menuProfil';
 
-// Composant pour le menu du profil utilisateur
+// Composant représentant le menu du profil utilisateur
 const MenuProfil = () => {
-  // Hooks pour la navigation et le contexte d'authentification
-  const navigation = useNavigation();
-  const { isUserLoggedIn, logOut } = useContext(AuthContext);
 
-  // Rendu du menu profil
+  // Utilisation des hooks pour la navigation et l'accès au contexte d'authentification
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  // Récupération des fonctions et états depuis le contexte d'authentification
+  const { isUserLoggedIn, logOut, userId } = useContext(AuthContext);
+
+  // Titres par défaut pour les différentes routes
+  const defaultTitles = {
+    Home: 'Accueil',
+    Login: 'Connexion',
+    SignUp: 'Inscription',
+    Location: 'Mes Locations',
+    CreateLocation: 'Création de Location',
+    ApLocation: 'Aperçus Final',
+    Profil: 'Mon Profile',
+    // ... autres écrans ...
+  };
+
+  // Récupération du titre de la route actuelle
+  const title = route.params?.title || defaultTitles[route.name] || route.name;
+
+  // Rendu du composant
   return (
-    <View style={stylesMenuProfil.iconContainer}>
-      <Menu>
-        { /* Déclencheur pour afficher le menu */ }
-        <MenuTrigger>
-          <Icon name={isUserLoggedIn ? "md-person" : "md-settings"} size={30} color="black"/>
-        </MenuTrigger>
-        { /* Options du menu */ }
-        <MenuOptions customStyles={{ optionsContainer: stylesMenuProfil.menuContainer }}>
-          {isUserLoggedIn ? (
-            <>
-              { /* Naviguer vers le profil */ }
-              <MenuOption onSelect={() => {
-                console.log("Bouton Profile");
-                // navigation.navigate('Profile');
-              }}
-                text='Profile' />
-              { /* Les paramètres utilisateur */ }
-              <MenuOption onSelect={() => {
-                console.log("Bouton Options");
-                // fonction pour les options
-              }}
-                text='Options' />
-              { /* Pour se déconnecter */ }
-              <MenuOption onSelect={logOut} text='Déconnexion' />
-            </>
-          ) : (
-            <>
-              { /* Pour naviguer vers la connexion */ }
-              <MenuOption onSelect={() => navigation.navigate('Login')} text='Connexion' />
-              { /* Pour naviguer vers la connexion */ }
-              <MenuOption onSelect={() => navigation.navigate('SignUp')} text='Inscription' />
-              { /* Les paramètres utilisateur */ }
-              <MenuOption onSelect={() => {/* fonction pour les options */}} text='Options'/>
-            </>
-          )}
-        </MenuOptions>
-      </Menu>
+    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+
+      {/* Icône pour naviguer vers l'accueil */}
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Icon name="md-home" size={30} color="black" style={{ marginLeft: 0 }} />
+      </TouchableOpacity>
+
+      {/* Affichage du titre de la page actuelle */}
+      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{title}</Text>
+
+      <View style={menuProfil.iconContainer}>
+        <Menu>
+          {/* Icône pour déclencher l'affichage du menu */}
+          <MenuTrigger>
+            <Icon name={isUserLoggedIn ? "md-person" : "md-settings"} size={30} color="black" style={{ marginRight: 10 }}/>
+          </MenuTrigger>
+
+          {/* Options du menu */}
+          <MenuOptions customStyles={{ optionsContainer: menuProfil.menuContainer }}>
+            {isUserLoggedIn ? (
+              <>
+                {/* Options pour les utilisateurs connectés */}
+                <MenuOption onSelect={() => {
+                  navigation.navigate('Profil');
+                  console.log("Bouton Profile");
+                }}
+                  text={userId || 'Profile'} />
+
+                <MenuOption onSelect={() => {
+                  navigation.navigate('Location');
+                  console.log("Bouton Mes Locations");
+                }}
+                  text='Mes Locations' />
+
+                <MenuOption onSelect={() => {
+                  console.log("Bouton Options");
+                  // fonction pour les options
+                }}
+                  text='Options' />
+
+                <MenuOption onSelect={ () => {
+                  logOut();
+                  navigation.navigate('Home');
+                  console.log("Déconnexion");
+                }}
+                  text='Déconnexion' />
+              </>
+            ) : (
+              <>
+                {/* Options pour les utilisateurs non connectés */}
+                <MenuOption onSelect={() => navigation.navigate('Login')} text='Connexion' />
+                <MenuOption onSelect={() => navigation.navigate('SignUp')} text='Inscription' />
+              </>
+            )}
+          </MenuOptions>
+
+        </Menu>
+      </View>
     </View>
   );
 };
 
+// Exportation du composant pour une utilisation dans d'autres fichiers
 export default MenuProfil;
