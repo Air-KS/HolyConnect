255e25cae6739452acc9aee23009745794996a7b
Air-KS <kevinrogeret@gmail.com>
2023-10-16 13:12:33 +0200

    Ajoute d'une barre de scrolling

---
+++ Application/screen/scrollView.js
@@ -0,0 +1,15 @@
+// src/styles/scrollView.js
+
+// Importation de la dépendance nécessaire pour créer des styles
+import { StyleSheet } from 'react-native';
+
+// Définition des styles pour les vues défilables (ScrollView)
+const scrollView = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: '#A6D2D2',
+  },
+});
+
+// Exportation des styles pour une utilisation dans d'autres fichiers
+export default scrollView;
