b48674de1ea616fba403c9eeb9f3b7c411d966cd
Air-KS <kevinrogeret@gmail.com>
2023-10-16 11:40:42 +0200

    Amélioration du code pour une meilleure lisibilité et ajout de commentaires

--- Application/App.js
+++ Application/App.js
@@ -1,35 +1,35 @@
 // App.js
 
-// Importation des modules nécessaires
+// Importation des dépendances nécessaires
 import React, { useState, useEffect, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as Font from 'expo-font';
-import HomeScreen from './src/navigation/homeScreen';
-import LoginScreen from './src/navigation/loginScreen';
-import SignUpScreen from './src/navigation/signUpScreen';
-import MenuProfil from './src/components/menuProfil';
 import { MenuProvider } from 'react-native-popup-menu';
 import { AuthProvider, AuthContext } from './src/contexts/AuthContext';
+import Home from './src/navigation/home';
+import Login from './src/navigation/login';
+import SignUp from './src/navigation/signUp';
+import MenuProfil from './src/components/menuProfil';
+import Location from './src/navigation/location';
+import createLocation from './src/navigation/creacteLocation';
+import apLocation from './src/navigation/apLocation';
+import Profil from './src/navigation/profil';
 
-// Initialisation du navigateur
+// Initialisation du navigateur Stack
 const Stack = createStackNavigator();
 
 // Composant principal de l'application
 export default function App() {
 
-  // État pour le chargement des polices
+  // État pour vérifier si les polices sont chargées
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  // Chargement des polices lors du démarrage de l'application
+  // Chargement des polices lors du montage du composant
   useEffect(() => {
     async function loadFonts() {
       try {
         await Font.loadAsync({
-          'MysticeTimes': require('./assets/fonts/MysticeTimes.otf'),
-          'CartoonStar': require('./assets/fonts/CartoonStar.ttf'),
-          'CartoonEmpire': require('./assets/fonts/CartoonEmpire.otf'),
-          'CartoonComical': require('./assets/fonts/CartoonComical.ttf'),
           'CheeseBurger': require('./assets/fonts/CheeseBurger.otf'),
         });
         setFontsLoaded(true);
@@ -44,48 +44,102 @@ export default function App() {
 
   // Si les polices ne sont pas chargées, ne rien afficher
   if (!fontsLoaded) {
-    return null; // Ou retournez un composant de chargement si vous en avez un
+    return null;
   }
 
-// comment
+  // Rendu du composant principal
   return (
     <AuthProvider>
       <MenuProvider>
         <NavigationContainer>
-          {/* Comment */}
+
+          {/* Configuration du navigateur Stack */}
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen
               name="Home"
-              component={HomeScreen}
+              component={Home}
               options={{
-                headerTitle: "HolyConnect - Home",
+                headerTitle: '',
                 headerStyle: {backgroundColor: '#E9E9E9'},
                 headerShown: true,
                 headerRight: () => <MenuProfil />
               }}
             />
-            {/* comment */}
+
+            {/* Écran de connexion */}
             <Stack.Screen
               name="Login"
-              component={LoginScreen}
+              component={Login}
               options={{
-                headerTitle: "Login",
+                headerTitle: '',
                 headerStyle: {backgroundColor: '#E9E9E9'},
                 headerShown: true,
                 headerRight: () => <MenuProfil />
               }}
             />
+
+            {/* Écran d'inscription */}
             <Stack.Screen
               name="SignUp"
-              component={SignUpScreen}
+              component={SignUp}
               options={{
-                headerTitle: "Inscription",
+                headerTitle: '',
                 headerStyle: {backgroundColor: '#E9E9E9'},
                 headerShown: true,
                 headerRight: () => <MenuProfil />
               }}
             />
+
+            {/* Écran de location */}
+            <Stack.Screen
+              name="Location"
+              component={Location}
+              options={{
+                headerTitle:'',
+                headerStyle: {backgroundColor: '#E9E9E9'},
+                headerShown: true,
+                headerRight: () => <MenuProfil />
+              }}
+            />
+
+            {/* Écran de création de location */}
+            <Stack.Screen
+              name="CreateLocation"
+              component={createLocation}
+              options={{
+                headerTitle:'',
+                headerStyle: {backgroundColor: '#E9E9E9'},
+                headerShown: true,
+                headerRight: () => <MenuProfil />
+              }}
+            />
+
+            {/* Écran de location (Aperçus) */}
+            <Stack.Screen
+              name="ApLocation"
+              component={apLocation}
+              options={{
+                headerTitle:'',
+                headerStyle: {backgroundColor: '#E9E9E9'},
+                headerShown: true,
+                headerRight: () => <MenuProfil />
+              }}
+            />
+
+            {/* Écran de profile */}
+            <Stack.Screen
+              name="Profil"
+              component={Profil}
+              options={{
+                headerTitle:'',
+                headerStyle: {backgroundColor: '#E9E9E9'},
+                headerShown: true,
+                headerRight: () => <MenuProfil />
+              }}
+            />
+
             {/* ... autres écrans ... */}
+
           </Stack.Navigator>
         </NavigationContainer>
       </MenuProvider>
