e7c16ecd9a7cc4f7bc7b452c6fc1e55cdf349405
Air-KS <kevinrogeret@gmail.com>
2023-10-16 12:06:12 +0200

    Amélioration du code pour une meilleure lisibilité, le suivis utilisateur et commentaires.

--- Application/src/contexts/AuthContext.js
+++ Application/src/contexts/AuthContext.js
@@ -1,17 +1,26 @@
-// AuthContext.js
+// src/context/AuthContext.js
+
+// Importation des dépendances nécessaires
 import React, { createContext, useState } from 'react';
 
+// Création du contexte d'authentification
 export const AuthContext = createContext();
 
+// Composant fournisseur du contexte d'authentification
 export const AuthProvider = ({ children }) => {
+  // État pour suivre si l'utilisateur est connecté ou non
   const [isUserLoggedIn, setUserLoggedIn] = useState(false);
 
+  const [userId, setUserId] = useState(null);
+  // Fonction pour déconnecter l'utilisateur
   const logOut = () => {
     setUserLoggedIn(false);
+    setUserId(null);
   };
 
+  // Fournir l'état et les fonctions associées aux composants enfants via le contexte
   return (
-    <AuthContext.Provider value={{ isUserLoggedIn, setUserLoggedIn, logOut }}>
+    <AuthContext.Provider value={{ isUserLoggedIn, setUserLoggedIn, userId, setUserId, logOut }}>
       {children}
     </AuthContext.Provider>
   );
