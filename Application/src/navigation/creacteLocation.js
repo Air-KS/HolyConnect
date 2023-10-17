64e4daba5cc1c3a6f09525e977e6116c2e8147c3
Air-KS <kevinrogeret@gmail.com>
2023-10-17 19:31:02 +0200

    Mise à jours des nouvelle location dans Mes location

--- Application/src/navigation/creacteLocation.js
+++ Application/src/navigation/creacteLocation.js
@@ -1,50 +1,57 @@
 // src/navigation/createLocation.js
 
 // Importation des modules nécessaires
-import React, { useState } from 'react';
-import { View, ScrollView, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import React, { useState, useContext } from 'react';
+import { View, ScrollView, Text, TextInput, Button } from 'react-native';
+import { TabView, TabBar } from 'react-native-tab-view';
+import { AuthContext } from '../contexts/AuthContext';
 import scrollView from '../screens/scrollView';
 import tabStyle from '../styles/tabBar';
 import baseStyle from '../styles/baseStyle';
 import formStyle from '../styles/formStyle';
-import locationStyle from '../styles/locationStyle';
 
 // Les composants de chaque onglet
-function InfoGeneral({ titreLocation, setTitreLocation }) {
-
-  const [localTitre] = useState(titreLocation);
-
+function InfoGeneral({ localTitre, setLocalTitre }) {
     return (
-    <ScrollView style={scrollView.scrollView}>
-      <View style={[tabStyle.scene]}>
-        <Text>Informations générales</Text>
-        <Text style={{...baseStyle.text, fontSize: 20}}>Titre</Text>
-        <View style={formStyle.input}>
-          <TextInput
-            style={formStyle.text}
-            placeholder="Titre"
-            value={localTitre}
-            onChangeText={text => setTitreLocation(text)}
-            autoCapitalize="none"
-            selectionColor="#264A4A"
-            returnKeyType="next"
-
-          />
-        </View>
-
-      </View>
-    </ScrollView>
+        <ScrollView style={scrollView.scrollView}>
+            <View style={[tabStyle.scene]}>
+                <Text>Informations générales</Text>
+                <Text style={{ ...baseStyle.text, fontSize: 20 }}>Nom de location</Text>
+                <View style={formStyle.input}>
+                    <TextInput
+                        style={formStyle.text}
+                        placeholder="Nom de location"
+                        value={localTitre}
+                        onChangeText={setLocalTitre}
+                        autoCapitalize="none"
+                        selectionColor="#264A4A"
+                        returnKeyType="next"
+                    />
+                </View>
+            </View>
+        </ScrollView>
     );
 }
 
-function Adresse() {
+function Adresse({ address, setAddress }) {
     return (
-    <ScrollView style={scrollView.scrollView}>
-      <View style={[tabStyle.scene]}>
-        <Text>Adresse</Text>
-        </View>
-    </ScrollView>
+        <ScrollView style={scrollView.scrollView}>
+            <View style={[tabStyle.scene]}>
+                <Text>Adresse</Text>
+                <Text style={{ ...baseStyle.text, fontSize: 20 }}>Adresse Complète</Text>
+                <View style={formStyle.input}>
+                    <TextInput
+                        style={formStyle.text}
+                        placeholder="Adresse Complète"
+                        value={address}
+                        onChangeText={setAddress}
+                        autoCapitalize="none"
+                        selectionColor="#264A4A"
+                        returnKeyType="next"
+                    />
+                </View>
+            </View>
+        </ScrollView>
     );
 }
 
@@ -53,55 +60,54 @@ const initialLayout = { width: '100%' };
 // Composant représentant l'écran d'accueil
 function CreateLocation({ navigation }) {
 
-  const [titreLocation, setTitreLocation] = useState('');
+    const [localTitre, setLocalTitre] = useState('');
+    const [address, setAddress] = useState('');
 
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-      { key: 'infoGeneral', title: 'Info Générale' },
-      { key: 'adresse', title: 'Adresse' },
-      // ... Ajoute d'autres routes pour les autres onglets
-  ]);
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: 'infoGeneral', title: 'Info Générale' },
+        { key: 'adresse', title: 'Adresse' }
+    ]);
 
-  const renderScene = SceneMap({
-    infoGeneral: () => <InfoGeneral titreLocation={titreLocation} setTitreLocation={setTitreLocation} />,
-      adresse: Adresse,
+    const renderScene = ({ route }) => {
+        switch (route.key) {
+            case 'infoGeneral':
+                return <InfoGeneral localTitre={localTitre} setLocalTitre={setLocalTitre} />;
+            case 'adresse':
+                return <Adresse address={address} setAddress={setAddress} />;
+            default:
+                return null;
+        }
+    };
 
-      // ... Ajoute d'autres mappings pour les autres onglets
-  });
-
-  // Rendu du composant
-  return (
-    <View style={{ flex: 1 }}>
-        <TabView
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-            initialLayout={initialLayout}
-            renderTabBar={props => (
-                <TabBar
-                    {...props}
-                    style={{ backgroundColor: '#2A6FC7' }}
+    return (
+        <View style={{ flex: 1 }}>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={initialLayout}
+                renderTabBar={props => (
+                    <TabBar
+                        {...props}
+                        style={{ backgroundColor: '#2A6FC7' }}
+                    />
+                )}
+            />
+            <View style={{ padding: 30, backgroundColor: '#A6D2D2' }}>
+                <Button
+                    title="Sauvegarder"
+                    onPress={async () => {
+                      //await saveUserLocationFromFile(userId, localTitre);
+                      navigation.navigate('Location', {
+                          locationTitle: localTitre,
+                          locationAddress: address
+                      });
+                    }}
                 />
-            )}
-        />
-        <View style={{ padding: 30, backgroundColor: '#A6D2D2' }}>
-        <Button
-      title="Sauvegarder"
-      onPress={() => {
-        navigation.navigate('Location', { title: titreLocation });
-      }}
-    />
+            </View>
         </View>
-    </View>
-  );
+    );
 }
-
-const styles = StyleSheet.create({
-  scene: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-  }
-});
 
 export default CreateLocation;
