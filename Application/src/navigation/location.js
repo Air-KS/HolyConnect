3142397175a90da4678b82da427837e13546f502
Air-KS <kevinrogeret@gmail.com>
2023-10-25 12:45:27 +0200

    Ajout de commentaire pour

--- Application/src/navigation/location.js
+++ Application/src/navigation/location.js
@@ -1,52 +1,109 @@
 // src/navigation/location.js
 
 // Importation des dépendances nécessaires
+import { useState, useEffect, useContext } from 'react';
 import { View, Text, Image, TouchableOpacity, FlatList, Button, ScrollView } from 'react-native';
 import locationStyle from '../styles/locationStyle';
-import { useState, useEffect } from 'react';
+import { getUserLocations, deleteLocation } from '../utils/fileManager';
+import { AuthContext } from '../contexts/AuthContext';
 
 // Composant représentant l'écran de gestion des locations
 function LocationScreen({ route, navigation }) {
+  const { userId } = useContext(AuthContext);
+
+  // État pour stocker les locations de l'utilisateur
   const [locations, setLocations] = useState([]);
 
-  useEffect(() => {
-    if (route.params?.title) {
-      setLocations(prev => [...prev, route.params.title]);
-    }
-  }, [route.params?.title]);
+  // État pour afficher ou masquer le bouton de suppression
+  const [showDeleteButton, setShowDeleteButton] = useState(true);
 
+  // Effet pour charger les locations de l'utilisateur
+  useEffect(() => {
+    const loadUserLocations = async () => {
+      // Chargez les locations de l'utilisateur actuel (utilisez le userId du contexte)
+      const userLocations = await getUserLocations(userId);
+      setLocations(userLocations);
+    };
+
+    // Appel de la fonction de chargement
+    loadUserLocations();
+  }, [userId]);
+
+  // Effet pour ajouter une nouvelle location à la liste
+  useEffect(() => {
+    if (route.params?.locationTitle && !locations.includes(route.params?.locationTitle)) {
+      setLocations(prev => [...prev, route.params?.locationTitle]);
+    }
+  }, [route.params?.locationTitle]);
+
+  // Fonction pour gérer la suppression d'une location
+  const handleDeleteLocation = async (item) => {
+    console.log("Emplacement supprimé :", item.title);
+    try {
+      // Supprimer l'élément du fichier JSON
+      await deleteLocation(item.id);
+
+      // Mettre à jour la liste des locations (en excluant l'élément supprimé)
+      setLocations((prevLocations) => prevLocations.filter((location) => location.id !== item.id));
+    } catch (error) {
+      console.error("Erreur lors de la suppression de l'emplacement", error);
+    }
+  };
 
   // Rendu du composant
   return (
-    // Conteneur principal avec défilement
-
-
-  <View style={locationStyle.container}>
-    <Button title="Ajouter une nouvelle location" onPress={() => navigation.navigate('CreateLocation')} />
-
-      <FlatList
-        data={locations}
-        renderItem={({ item }) =>
-        <Text style={locationStyle.text}>{item}</Text>}
-        keyExtractor={(item, index) => index.toString()}
+    <View style={locationStyle.container}>
+      <Button
+        title="Ajouter une nouvelle location"
+        onPress={() => {
+          navigation.navigate('CreateLocation');
+        }}
       />
 
+      {/* Liste des locations */}
+      <FlatList
+        data={locations}
+        renderItem={({ item }) => (
+          <View style={locationStyle.listItem}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('UiInterface');
+                console.log("Location cliquée :", item.title);
+              }}
+            >
+              <Text style={locationStyle.text}>{item.title}</Text>
+            </TouchableOpacity>
+            <View style={locationStyle.buttonsContainer}>
+              <TouchableOpacity
+                style={locationStyle.button}
+                onPress={() => {
+                  // Action de modification
+                  // Vous pouvez naviguer vers la page de modification ici
+                }}
+              >
+                <Text style={locationStyle.buttonText}>Modifier</Text>
+              </TouchableOpacity>
 
-    {/* Section pour voir un aperçu de la location */}
-    <TouchableOpacity onPress={() => navigation.navigate('ApLocation')}>
-      <View style={locationStyle.textLocal}>
-        <Text style={locationStyle.text}>Voir un aperçus du rendu Final et voilà
-        que j'écris une longue phrase juste pour voir si ça fonctionne !
-        Je doute qu'il y aura autant de place disponible !</Text>
-      </View>
-    </TouchableOpacity>
+              {/* Bouton de suppression */}
+              {showDeleteButton && (
+                <TouchableOpacity
+                  style={locationStyle.button}
+                  onPress={() => {
+                    handleDeleteLocation(item);
+                  }}
+                >
+                  <Text style={{ ...locationStyle.buttonText, color: 'red' }}>Supprimer</Text>
+                </TouchableOpacity>
+              )}
 
-    {/* Section pour créer une nouvelle location */}
-    <Text style={locationStyle.text}>Créer une nouvelle Location</Text>
-    <TouchableOpacity onPress={() => navigation.navigate('CreateLocation')}>
-      <Image source={require('../../assets/images/Nouveau.png')} style={locationStyle.addLocation} />
-    </TouchableOpacity>
-  </View>
+            </View>
+          </View>
+        )}
+
+        // Fonction pour obtenir un identifiant unique pour chaque élément
+        keyExtractor={(item) => (item.id ? item.id.toString() : Math.random().toString())}
+      />
+    </View>
   );
 }
 
