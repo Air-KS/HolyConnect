47aa8325bcdfeda8cb8b99c58d60fcdabf39f928
Air-KS <kevinrogeret@gmail.com>
2023-10-25 12:23:13 +0200

    Ajout des nouveaux logs

--- Application/src/contexts/AuthContext.js
+++ Application/src/contexts/AuthContext.js
@@ -10,17 +10,45 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   // État pour suivre si l'utilisateur est connecté ou non
   const [isUserLoggedIn, setUserLoggedIn] = useState(false);
-
   const [userId, setUserId] = useState(null);
+  const [userPseudo, setUserPseudo] = useState(null);
+  const [userData, setUserData] = useState(null);
+
+  // Fonction pour connecter l'utilisateur
+  const login = async (user) => {
+    setUserLoggedIn(true);
+    setUserId(user.id);
+    setUserPseudo(user.pseudo);
+
+    // Chargez les données de l'utilisateur depuis le fichier JSON et stockez-les dans userData
+    const userDataFromJson = await loadUserDataFromFile(user.id); // Assurez-vous que loadUserDataFromFile est une fonction asynchrone
+    setUserData(userDataFromJson);
+  };
+
   // Fonction pour déconnecter l'utilisateur
   const logOut = () => {
     setUserLoggedIn(false);
     setUserId(null);
+    setUserPseudo(null);
+    setUserData(null);
+  };
+
+  // Fonction pour mettre à jour le pseudo de l'utilisateur
+  const updateUserPseudo = (pseudo) => {
+    setUserPseudo(pseudo);
+  };
+
+  // Fonction pour définir l'ID de l'utilisateur dans le contexte d'authentification
+  const setAuthContextUserId = (id) => {
+    setUserId(id);
   };
 
   // Fournir l'état et les fonctions associées aux composants enfants via le contexte
   return (
-    <AuthContext.Provider value={{ isUserLoggedIn, setUserLoggedIn, userId, setUserId, logOut }}>
+    <AuthContext.Provider value={{
+      isUserLoggedIn, userId, userPseudo, userData,
+      updateUserPseudo, setUserLoggedIn, setUserId, setAuthContextUserId, setUserPseudo, setUserData,
+      login, logOut }}>
       {children}
     </AuthContext.Provider>
   );
