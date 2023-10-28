cdd7c49e34e4a7a3a0dc489913c8a3c1ac7bf089
lenne sebastien <sebastienlenne@gmail.com>
2023-10-28 20:11:19 +0200

    ajoute fichier config.json / fichier de configuration de la base de donnees

---
+++ Application/Back-End/config/db.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  "database_hollyconnect_development",
+  "root",
+  "qwertyseb",
+  {
+    host: "127.0.0.1",
+    port: "3306",
+    dialect: "mysql",
+    // Autres options de configuration si nécessaires
+  },
+);
+
+// Vérification de la connexion à la base de données
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connexion à la base de données réussie.");
+  })
+  .catch((err) => {
+    console.error("Impossible de se connecter à la base de données :", err);
+  });
+
+module.exports = sequelize;
