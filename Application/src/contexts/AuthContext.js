756746fa85624b46771506819fc694958fcb7a9c
Air-KS <kevinrogeret@gmail.com>
2023-11-03 17:02:22 +0100

    Modif for update async and login

--- Application/src/contexts/AuthContext.js
+++ Application/src/contexts/AuthContext.js
@@ -2,6 +2,7 @@
 
 // Importation des dépendances nécessaires
 import React, { createContext, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Création du contexte d'authentification
 export const AuthContext = createContext();
@@ -13,22 +14,34 @@ export const AuthProvider = ({ children }) => {
   const [userId, setUserId] = useState(null);
   const [userPseudo, setUserPseudo] = useState(null);
   const [userData, setUserData] = useState(null);
+  const [token, setToken] = useState(null);
 
   // Fonction pour connecter l'utilisateur
   const login = async (user) => {
+    const token = await generateTokenForUser(user);
+    //localStorage.setItem('userToken', token);
+    await AsyncStorage.setItem('userToken', token);
+
     setUserLoggedIn(true);
     setUserId(user.id);
     setUserPseudo(user.pseudo);
   };
 
   // Fonction pour déconnecter l'utilisateur
-  const logOut = () => {
-    setUserLoggedIn(false);
-    setUserId(null);
-    setUserPseudo(null);
-    setUserData(null);
-  };
+  const logOut = async () => {
+    try {
+      // suppresssion du token lors de la deconnexion
+      await AsyncStorage.removeItem('userToken');
 
+      setUserLoggedIn(false);
+      setUserId(null);
+      setUserPseudo(null);
+      setUserData(null);
+      setToken(null);
+    } catch (err) {
+      console.error("Erreur lors de la deconnexion", err);
+    }
+  };
   // Fonction pour mettre à jour le pseudo de l'utilisateur
   const updateUserPseudo = (pseudo) => {
     setUserPseudo(pseudo);
@@ -42,7 +55,7 @@ export const AuthProvider = ({ children }) => {
   // Fournir l'état et les fonctions associées aux composants enfants via le contexte
   return (
     <AuthContext.Provider value={{
-      isUserLoggedIn, userId, userPseudo, userData,
+      isUserLoggedIn, userId, userPseudo, userData, token, setToken,
       updateUserPseudo, setUserLoggedIn, setUserId, setAuthContextUserId, setUserPseudo, setUserData,
       login, logOut }}>
       {children}
