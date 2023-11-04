5adf6744b41ebdbeb8f86b5ae991526d02256b39
Air-KS <kevinrogeret@gmail.com>
2023-11-04 13:45:29 +0100

    Add get, post, put and delete to cors, line 32

--- Application/serveur.js
+++ Application/serveur.js
@@ -1,3 +1,14 @@
+// fichier qui contient la logique des Midlewaqare, permet de lancer le serveur: node serveur.js
+// version: 1.0.0
+// Auteur: LENNE Sebastien
+// Mise à jours, Version: 1.5.2 : ROGERET Kevin
+
+/*
+  Mise à jours : [1.5.2]
+  Modifi l'ordre des approprié des Middleware
+  Ajoute les methods : get, post, put, delete dans le cors
+*/
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const apirouter = require("./Back-End/api/apirouter").router;
@@ -17,7 +28,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware CORS
-app.use(cors());
+app.use(cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 // Middleware pour les logs
 app.use((req, res, next) => {
