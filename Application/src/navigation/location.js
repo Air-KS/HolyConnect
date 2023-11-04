fcd133a55370727ccee534332ceb9a9f200db48e
Air-KS <kevinrogeret@gmail.com>
2023-11-04 13:49:29 +0100

    Modif handleDeleteLocation function for delete location by ID

--- Application/src/navigation/location.js
+++ Application/src/navigation/location.js
@@ -32,6 +32,7 @@ function LocationScreen({ route, navigation }) {
 
       if (response.ok) {
         const data = await response.json();
+        console.log('Locations chargées:', data);
         setLocations(data);
       } else {
         console.error('Erreur de statut:', response.status);
@@ -43,6 +44,42 @@ function LocationScreen({ route, navigation }) {
     }
   };
 
+  // Fonction pour modifier la location
+  const handleUpdateLocation = async (item, newDetails) => {
+    const token = await AsyncStorage.getItem('userToken');
+    if (!token) {
+      console.error("Le token de l'utilisateur est absent ou vide.");
+      return;
+    }
+
+    try {
+      const response = await fetch(`http://192.168.1.17:3000/api/homelocation/updateloc`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: item.id,
+          ...newDetails // Ici, tu devras passer les nouvelles informations à mettre à jour
+        }),
+      });
+
+      if (response.ok) {
+        // Mise à jour de l'état local après la mise à jour réussie
+        setLocations(prevLocations =>
+          prevLocations.map(location =>
+            location.id === item.id ? { ...location, ...newDetails } : location
+          )
+        );
+      } else {
+        console.error('Erreur lors de la mise à jour:', await response.text());
+      }
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour de l\'emplacement:', error);
+    }
+  };
+
   // Fonction pour gérer la suppression d'une location
   const handleDeleteLocation = async (item) => {
     const token = await AsyncStorage.getItem('userToken');
@@ -51,17 +88,20 @@ function LocationScreen({ route, navigation }) {
       return;
     }
 
+    console.log("Tentative de suppression de la location avec l'ID:", item.id); // Ajouter ce log
+
     try {
-      const response = await fetch(`http://192.168.1.17:3000/api/homelocation/deleteloc`, {
+      const response = await fetch(`http://192.168.1.17:3000/api/homelocation/deleteloc/${item.id}`, {
         method: 'DELETE',
         headers: {
           'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
+          //'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ id: item.id }),
+        //body: JSON.stringify({ id: item.id }),
       });
 
       if (response.ok) {
+        console.log(`Location avec ID ${item.id} supprimée.`);
         setLocations(prevLocations => prevLocations.filter(location => location.id !== item.id));
       } else {
         console.error('Erreur lors de la suppression:', await response.text());
@@ -78,15 +118,15 @@ function LocationScreen({ route, navigation }) {
 
 // Dans LocationScreen
 useEffect(() => {
-  if (route.params?.namelocation) {
-    const newLocation = {
-      id: Date.now(),
-      title: route.params.namelocation, // Utilise le nom de la location comme titre
-      // Ajoute d'autres détails si nécessaire
-    };
-    setLocations(prevLocations => [...prevLocations, newLocation]);
+  if (route.params?.isEditing) {
+    handleUpdateLocation({
+      id: route.params.id,
+      namelocation: route.params.namelocation,
+      adresslocation: route.params.adresslocation,
+      infolocation: route.params.infolocation,
+    })
   }
-}, [route.params?.namelocation]); // Dépendance à route.params.namelocation
+}, [route.params]); // Dépendance à route.params.namelocation
 
   return (
     <View style={locationStyle.container}>
@@ -110,14 +150,21 @@ useEffect(() => {
               <Text style={locationStyle.text}>{item.namelocation}</Text>
             </TouchableOpacity>
             <View style={locationStyle.buttonsContainer}>
-              <TouchableOpacity
-                style={locationStyle.button}
-                onPress={() => {
-                  // Mettre en place la logique de modification ici
-                }}
-              >
-                <Text style={locationStyle.buttonText}>Modifier</Text>
-              </TouchableOpacity>
+            <TouchableOpacity
+              style={locationStyle.button}
+              onPress={() => {
+              // Directement naviguer et passer les paramètres nécessaires
+              navigation.navigate('CreateLocation', {
+                id: item.id, // Passer l'ID pour la mise à jour
+                namelocation: item.namelocation,
+                adresslocation: item.adresslocation,
+                infolocation: item.infolocation,
+                isEditing: true,
+              });
+            }}
+            >
+            <Text style={locationStyle.buttonText}>Modifier</Text>
+            </TouchableOpacity>
 
               {showDeleteButton && (
                 <TouchableOpacity
